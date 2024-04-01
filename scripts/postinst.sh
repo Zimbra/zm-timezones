a8cebc6a6cd81b37302fdd1b6631a49b5122a07f
UmaShankar Avagadda <umagmrit@gmail.com>
2024-04-01 13:57:22 +0000

    ZCS-14844:add preinst.sh,postinst.sh for restrict write access for /opt/zimbra/jetty_base/webapps

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo "**** apply restrict write access for /opt/zimbra/jetty_base/webapps/ ****(zimbra-timezone-data)"
-chattr +i -R /opt/zimbra/jetty_base/webapps/
+if lsattr -d /path/to/directory | grep -q 'i'; then
+    echo "Immutable attribute already set on the /opt/zimbra/jetty_base/webapps/ (zimbra-timezone-data)"
+else
+        echo "**** apply restrict write access for /opt/zimbra/jetty_base/webapps/ (zimbra-timezone-data)****"
+        chattr +i -R /opt/zimbra/jetty_base/webapps/
+fi
